b9cc1c72aa6a45eb1986d340cfc59b2f22a4d0d5
Matthias Van Parijs <matvp91@gmail.com>
2026-04-06 21:10:58 +0200

    feat: expand event system for controller architecture

--- lib/events.ts
+++ lib/events.ts
@@ -1,19 +1,52 @@
+// lib/events.ts
+import type { Manifest, SelectionSet, Track } from "./types/manifest";
+
 export const Events = {
+  MANIFEST_LOADING: "manifestLoading",
+  MANIFEST_PARSED: "manifestParsed",
   MEDIA_ATTACHING: "mediaAttaching",
   MEDIA_ATTACHED: "mediaAttached",
   MEDIA_DETACHED: "mediaDetached",
+  SEGMENT_LOADED: "segmentLoaded",
+  BUFFER_APPENDED: "bufferAppended",
+  BUFFER_EOS: "bufferEos",
 };
 
-type MediaAttachingEvent = {
+export type ManifestLoadingEvent = {
+  url: string;
+};
+
+export type ManifestParsedEvent = {
+  manifest: Manifest;
+};
+
+export type MediaAttachingEvent = {
   media: HTMLMediaElement;
 };
 
-type MediaAttachedEvent = {
+export type MediaAttachedEvent = {
   media: HTMLMediaElement;
+  mediaSource: MediaSource;
+};
+
+export type SegmentLoadedEvent = {
+  selectionSet: SelectionSet;
+  track: Track;
+  data: ArrayBuffer;
+  segmentIndex: number;
+};
+
+export type BufferAppendedEvent = {
+  selectionSet: SelectionSet;
 };
 
 export interface EventMap {
+  [Events.MANIFEST_LOADING]: (event: ManifestLoadingEvent) => void;
+  [Events.MANIFEST_PARSED]: (event: ManifestParsedEvent) => void;
   [Events.MEDIA_ATTACHING]: (event: MediaAttachingEvent) => void;
   [Events.MEDIA_ATTACHED]: (event: MediaAttachedEvent) => void;
   [Events.MEDIA_DETACHED]: undefined;
+  [Events.SEGMENT_LOADED]: (event: SegmentLoadedEvent) => void;
+  [Events.BUFFER_APPENDED]: (event: BufferAppendedEvent) => void;
+  [Events.BUFFER_EOS]: undefined;
 }
