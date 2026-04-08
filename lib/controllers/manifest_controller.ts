13bac6331c5c957b96f986d650b289171e2e5eb0
Matthias Van Parijs <matvp91@gmail.com>
2026-04-08 21:36:59 +0200

    refactor: ManifestController uses lastRequest pattern

--- lib/controllers/manifest_controller.ts
+++ lib/controllers/manifest_controller.ts
@@ -3,11 +3,12 @@ import type { ManifestLoadingEvent } from "../events";
 import { Events } from "../events";
 import type { NetworkService } from "../net/network_service";
 import { RequestType } from "../net/network_service";
-import { Request } from "../net/request";
+import type { Request } from "../net/types";
+import { ABORTED } from "../net/types";
 import type { Player } from "../player";
 
 export class ManifestController {
-  private request_: Request | null = null;
+  private lastRequest_: Request<"text"> | null = null;
 
   constructor(
     private player_: Player,
@@ -17,27 +18,25 @@ export class ManifestController {
   }
 
   destroy() {
-    this.request_?.cancel();
-    this.request_ = null;
+    if (this.lastRequest_) {
+      this.networkService_.cancel(this.lastRequest_);
+    }
     this.player_.off(Events.MANIFEST_LOADING, this.onManifestLoading_);
   }
 
   private onManifestLoading_ = async (event: ManifestLoadingEvent) => {
-    const { url } = event;
-
-    this.request_ = this.networkService_.request(
+    this.lastRequest_ = this.networkService_.request(
       RequestType.MANIFEST,
-      new Request(url),
+      event.url,
+      "text",
     );
 
-    const response = await this.request_.promise;
-    this.request_ = null;
-
-    if (!response) {
+    const response = await this.lastRequest_.promise;
+    if (response === ABORTED) {
       return;
     }
 
-    const manifest = await parseManifest(response.text(), url);
+    const manifest = await parseManifest(response.data, event.url);
     this.player_.emit(Events.MANIFEST_PARSED, { manifest });
   };
 }
