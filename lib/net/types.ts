bfd3c2a1b218956a7326385ac6c453df36e28062
Matthias Van Parijs <matvp91@gmail.com>
2026-04-08 21:31:04 +0200

    refactor: replace resolversSymbol with network types in types.ts

--- lib/net/types.ts
+++ lib/net/types.ts
@@ -1,1 +1,28 @@
-export const resolversSymbol = Symbol("promiseResolvers");
+export type HttpMethod = "GET" | "POST";
+
+export type ResponseType = "arrayBuffer" | "text";
+
+export const ABORTED: unique symbol = Symbol("ABORTED");
+
+type ResponseData = {
+  arrayBuffer: ArrayBuffer;
+  text: string;
+};
+
+export type Request<T extends ResponseType> = {
+  url: string;
+  method: HttpMethod;
+  headers: Headers;
+  responseType: T;
+  inFlight: boolean;
+  cancelled: boolean;
+  promise: Promise<Response<T> | typeof ABORTED>;
+};
+
+export type Response<T extends ResponseType> = {
+  request: Request<T>;
+  status: number;
+  headers: Headers;
+  data: ResponseData[T];
+  timeElapsed: number;
+};
