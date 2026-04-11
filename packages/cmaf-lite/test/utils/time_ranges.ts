6e546c4e667de5d4ca5c3d1cdc065d0625fe1b0a
Matthias Van Parijs <matvp91@gmail.com>
2026-04-11 17:06:02 +0200

    test: add createTimeRanges helper

---
+++ packages/cmaf-lite/test/utils/time_ranges.ts
@@ -0,0 +1,23 @@
+/**
+ * Creates a TimeRanges-compatible object from pairs.
+ */
+export function createTimeRanges(
+  ...ranges: [number, number][]
+): TimeRanges {
+  const sorted = [...ranges].sort((a, b) => a[0] - b[0]);
+  return {
+    length: sorted.length,
+    start(i: number) {
+      if (i < 0 || i >= sorted.length) {
+        throw new DOMException("Index out of bounds");
+      }
+      return sorted[i]![0];
+    },
+    end(i: number) {
+      if (i < 0 || i >= sorted.length) {
+        throw new DOMException("Index out of bounds");
+      }
+      return sorted[i]![1];
+    },
+  };
+}
