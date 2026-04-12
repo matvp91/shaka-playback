a2cd54f1c0d21f286f47fef0426cd0b8c847913a
Matthias Van Parijs <matvp91@gmail.com>
2026-04-12 23:22:10 +0200

    feat(demo): add StreamItem component

---
+++ packages/demo/src/components/stream-list/StreamItem.tsx
@@ -0,0 +1,26 @@
+import type { Stream } from "cmaf-lite";
+import { MediaType } from "cmaf-lite";
+import { formatBandwidth } from "../../utils/stream";
+
+type StreamItemProps = {
+  stream: Stream;
+  active: boolean;
+};
+
+export function StreamItem({ stream, active }: StreamItemProps) {
+  return (
+    <div className="flex items-center gap-2">
+      {active && <span>●</span>}
+      {stream.type === MediaType.VIDEO ? (
+        <span>
+          {stream.width}x{stream.height} · {formatBandwidth(stream.bandwidth)}{" "}
+          · {stream.codec}
+        </span>
+      ) : (
+        <span>
+          {formatBandwidth(stream.bandwidth)} · {stream.codec}
+        </span>
+      )}
+    </div>
+  );
+}
