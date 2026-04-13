13ff4c9e810fd37b888bfadb8cb58d411408fe4a
Matthias Van Parijs <matvp91@gmail.com>
2026-04-13 22:59:34 +0200

    fix: Do not add hashes to chunks

--- packages/cmaf-lite/tsdown.config.ts
+++ packages/cmaf-lite/tsdown.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
   format: "esm",
   noExternal: ["txml"],
   // TODO(matvp): Create priority in dev, we currently
-  // do not clean due to demo relying on dist
+  // do not clean due to demo relying on dist.
   clean: false,
   plugins: [relativeCiAgent()],
+  // Do not hash chunks, they mess with bundle analyzer.
+  hash: false,
 });
